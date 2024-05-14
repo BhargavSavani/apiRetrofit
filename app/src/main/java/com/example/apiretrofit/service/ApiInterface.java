f5096bff039b8f1e86ebd93974e351d6f0e56bfd
bhargav savani <bhargavsavani1501@gmil.com>
2024-05-14 15:42:02 +0530

    my first commit

--- app/src/main/java/com/example/apiretrofit/service/ApiInterface.java
+++ app/src/main/java/com/example/apiretrofit/service/ApiInterface.java
@@ -1,14 +1,19 @@
 package com.example.apiretrofit.service;
 
+import android.view.PixelCopy;
+
 import com.example.apiretrofit.model.UserModel;
 
 import java.util.List;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
+
     @GET("/products")
     Call<List<UserModel>> getUser();
 
